77e5d3aa78cae1e0833c49a4aa430632405501f8
Titan <68808479+titan3755@users.noreply.github.com>
2024-11-22 12:19:18 +0600

    Create Main.kt

---
+++ basics/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val a: Int = 1
+    val b: Int = 500
+    val c: Int = 1000
+    var d: Int = 200
+    println("a = $a, b = $b, c = $c, d = $d")
+    d = 300
+    println("a = $a, b = $b, c = $c, d = $d")
+}
